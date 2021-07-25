14f9458362fb5a83a0fe5138ee9d1f6e829a91db
Frank Robnik <frank.robnik@gmail.com>
2021-07-25 16:34:25 +0200

    change folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 'use strict';
 
-const { series, parallel, src, dest, watch } = require('gulp');
-const del = require('del');
+const { parallel, src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
@@ -9,64 +8,46 @@ const cssnano = require('cssnano');
 const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync').create();
 
-function clean() {
-  return del(['public/**', '!public']);
-}
-
 function css() {
-  return src('./src/sass/**/*.scss')
+  return src('./sass/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([ autoprefixer(), cssnano() ]))
     .pipe(sourcemaps.write('.'))
-    .pipe(dest('./public/css'))
+    .pipe(dest('./css'))
     .pipe(browserSync.stream({match: '**/*.css'}));
 }
 
 function javascript() {
-  return src('./src/**/*.js')
-    .pipe(dest('./public'))
+  return src('./js/**/*.js')
+    .pipe(dest('./js'))
     .pipe(browserSync.stream());
 }
 
 function html() {
-  return src('./src/**/*.html')
-    .pipe(dest('./public'))
-    .pipe(browserSync.stream());
-}
-
-function images() {
-  return src([
-      './src/images/*.jpg', 
-      './src/images/*.png',
-      './src/images/*.gif',
-      './src/images/*.svg',
-      './src/images/*.webp'
-    ])
-    .pipe(dest('./public/images'))
+  return src('./**/*.html')
+    .pipe(dest('./'))
     .pipe(browserSync.stream());
 }
 
 function develop(){
   browserSync.init({
-    server: "./public/"
+    server: "./"
   });
 
-  watch('src/sass/*.scss', { ignoreInitial: false }, css);
-  watch('src/js/*.js', { ignoreInitial: false }, javascript);
-  watch('src/**/*.html',  { ignoreInitial: false }, html);
-  watch('src/images/*.*',  { ignoreInitial: false }, images);
+  watch('sass/*.scss', { ignoreInitial: false }, css);
+  watch('js/*.js', { ignoreInitial: false }, javascript);
+  watch('**/*.html',  { ignoreInitial: false }, html);
 
   watch([
-    'public/css/*.css',
-    'public/js/*.js',
-    'public/**/*.html',
-    'public/images/*.*'
+    //'css/*.css',
+    //'js/*.js',
+    '**/*.html',
+    'images/*.*'
   ]).on('change', browserSync.reload);
 
 }
 
 
-exports.build = series(clean, parallel(css, javascript, html, images));
-exports.clean = clean;
+exports.build = parallel(css, javascript, html, images);
 exports.develop = develop;
